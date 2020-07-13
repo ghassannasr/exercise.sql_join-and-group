ba31b7a655ec13deab7f9962d0a504a554a9e060
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-07-13 16:09:57 -0400

    completed part5

--- part5.sql
+++ part5.sql
@@ -1,8 +1,8 @@
-SELECT concat(e.lastName, ', ', e.firstName) AS 'Sales Rep', SUM(od.quantityOrdered) AS '# Orders', SUM(od.quantityOrdered * od.priceEach) AS 'Total Sales'
+SELECT concat(e.lastName, ', ', e.firstName) AS 'Sales Rep', COUNT(od.quantityOrdered) AS '# Orders', IFNULL(SUM(od.quantityOrdered * od.priceEach), 0) AS 'Total Sales'
 from employees e
 LEFT JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
 LEFT JOIN orders o ON c.customerNumber = o.customerNumber
 LEFT JOIN orderdetails od ON o.orderNumber = od.orderNumber
 WHERE e.jobTitle = 'Sales Rep'
-GROUP BY e.lastName
+GROUP BY e.lastName, e.firstName
 ORDER BY SUM(od.quantityOrdered * od.priceEach) DESC
